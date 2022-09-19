095525e73a457c2b74e7864d6915f214af18d014
Nikola <nikola.vladimirov@codeus.rs>
2022-09-20 00:07:06 +0200

    Hardcoded dogs records.

---
+++ src/dogs/index.js
@@ -0,0 +1,177 @@
+export const dogs = [
+  {
+    id: 0,
+    name: 'Leo',
+    breed: 'Chihuahua',
+    registration: 'Sep 29, 2020',
+    timeline: [
+      'Dog found romaing on streets on Sep 26, 2020',
+      'Joined Animal Sheter on Sep 29, 2020'
+    ],
+    image: 'https://images.dog.ceo/breeds/chihuahua/n02085620_2793.jpg'
+  },
+  {
+    id: 1,
+    name: 'Niga',
+    breed: 'Chihuahua',
+    registration: '',
+    timeline: [
+      'Dog found romaing on streets on Mar 14, 2019',
+      'Joined Animal Sheter on Mar 14, 2020'
+    ],
+    image: 'https://images.dog.ceo/breeds/chihuahua/n02085620_8491.jpg'
+  },
+  {
+    id: 2,
+    name: 'Azra',
+    breed: 'Chihuahua',
+    registration: 'Jun 24, 2021',
+    timeline: [
+      'Dog found romaing on streets on Jun 21, 2021',
+      'Joined Animal Sheter on Jun 24, 2021'
+    ],
+    image: 'https://images.dog.ceo/breeds/chihuahua/n02085620_11140.jpg'
+  },
+  {
+    id: 3,
+    name: 'Rocky',
+    breed: 'Husky',
+    registration: 'Feb 25, 2019',
+    timeline: [
+      'Dog found romaing on streets on Feb 5, 2019',
+      'Joined Animal Sheter on Feb 25, 2019'
+    ],
+    image: 'https://images.dog.ceo/breeds/husky/n02110185_8360.jpg'
+  },
+  {
+    id: 4,
+    name: 'Mike',
+    breed: 'Husky',
+    registration: 'Jan 18, 2021',
+    timeline: [
+      'Dog found romaing on streets on Jan 2, 2021',
+      'Joined Animal Sheter on Jan 18, 2021'
+    ],
+    image: 'https://images.dog.ceo/breeds/husky/n02110185_6850.jpg'
+  }, {
+    id: 5,
+    name: 'Anhelina',
+    breed: 'Pitbull',
+    registration: 'Jan 29, 2021',
+    timeline: [
+      'Dog found romaing on streets on Jan 29, 2021',
+      'Joined Animal Sheter on Jan 29, 2021'
+    ],
+    image: 'https://images.dog.ceo/breeds/pitbull/pitbull%20dog.jpg'
+  },
+  {
+    id: 6,
+    name: 'Polo',
+    breed: 'Fox Terrier',
+    registration: 'Jun 24, 2021',
+    timeline: [
+      'Dog found romaing on streets on Jun 3, 2021',
+      'Joined Animal Sheter on Jun 24, 2021'
+    ],
+    image: 'https://images.dog.ceo/breeds/terrier-fox/n02095314_2338.jpg'
+  },
+  {
+    id: 7,
+    name: 'Bob',
+    breed: 'Fox Terrier',
+    registration: 'Dec. 7, 2022',
+    timeline: [
+      'Dog found romaing on streets on Dec. 2, 2022',
+      'Joined Animal Sheter on Dec. 7, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/terrier-fox/n02095314_2904.jpg'
+  },
+  {
+    id: 8,
+    name: 'Madra',
+    breed: 'Fox Terrier',
+    registration: 'Jan. 4, 2023',
+    timeline: [
+      'Dog found romaing on streets on Jan. 4, 2023',
+      'Joined Animal Sheter on Jan. 4, 2023'
+    ],
+    image: 'https://images.dog.ceo/breeds/terrier-fox/n02095314_3343.jpg'
+  },
+  {
+    id: 9,
+    name: 'Sia',
+    breed: 'Fox Terrier',
+    registration: 'Sep. 12, 2022',
+    timeline: [
+      'Dog found romaing on streets on Sep. 1, 2022',
+      'Joined Animal Sheter on Sep. 12, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/terrier-fox/n02095314_1278.jpg'
+  },
+  {
+    id: 10,
+    name: 'Rundy',
+    breed: 'Boxer',
+    registration: 'Sep. 15, 2022',
+    timeline: [
+      'Dog found romaing on streets on Sep. 5, 2022',
+      'Joined Animal Sheter on Sep. 15, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/boxer/n02108089_2367.jpg'
+  },
+  {
+    id: 11,
+    name: 'Jake',
+    breed: 'Boxer',
+    registration: 'Jan. 22, 2022',
+    timeline: [
+      'Dog found romaing on streets on Jan. 1, 2022',
+      'Joined Animal Sheter on Jan. 22, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/boxer/n02108089_2917.jpg'
+  },
+  {
+    id: 12,
+    name: 'Mo',
+    breed: 'Boxer',
+    registration: '',
+    timeline: [
+      'Dog found romaing on streets on 5yh June 2021',
+      'Joined Animal Sheter on 6th June 2021'
+    ],
+    image: 'https://images.dog.ceo/breeds/boxer/n02108089_268.jpg'
+  },
+  {
+    id: 13,
+    name: 'Pontos',
+    breed: 'Boxer',
+    registration: 'Apr. 11, 2022',
+    timeline: [
+      'Dog found romaing on streets on Apr. 11, 2022',
+      'Joined Animal Sheter on 6th Apr. 11, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/boxer/n02108089_922.jpg'
+  },
+  {
+    id: 14,
+    name: 'Parker',
+    breed: 'Boxer',
+    registration: 'Apr. 11, 2022',
+    timeline: [
+      'Dog found romaing on streets on Apr. 1, 2022',
+      'Joined Animal Sheter on 6th Apr. 11, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/boxer/n02108089_2815.jpg'
+  },
+  {
+    id: 15,
+    name: 'Ray',
+    breed: 'Boxer',
+    registration: 'Apr. 11, 2022',
+    timeline: [
+      'Dog found romaing on streets on Apr. 2, 2022',
+      'Joined Animal Sheter on Apr. 11, 2022'
+    ],
+    image: 'https://images.dog.ceo/breeds/boxer/n02108089_7319.jpg'
+  }
+]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,19 @@
 </template>
 
 <script>
+import { onMounted } from '@vue/runtime-core'
+import axios from 'axios'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  setup () {
+    // const apiKey = '?api_key=live_rvw1WL8fCnFzHWM3cPuSNLzAJfIJOAhCRaEpvQNiXRXpLRbpelhhnNTdsZ0CKad4'
+    const url = 'https://thedogapi.com/v1/images'
+
+    onMounted(() => {
+      axios.get(url).then((res) => {
+        console.log(res)
+      })
+    })
+  }
 }
 </script>
