43d078e456dc12131bae2750a7f7e97ce0a97587
Nikola <nikola.vladimirov@codeus.rs>
2022-09-22 13:40:02 +0200

    Fixed

--- src/main.js
+++ src/main.js
@@ -5,7 +5,5 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import './assets/main.scss'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-createApp(App).use(store, axios, VueAxios, ElementUI).use(router).mount('#app')
+createApp(App).use(store, axios, VueAxios).use(router).mount('#app')
